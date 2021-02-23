ad1a2a72dd36dd807c7780ae50854f5bf6f27756
Kelly Cruz <kelly.cruz@rebase.com.br>
2021-02-23 15:55:24 -0300

    Calcula o imc do paciente

--- js/principal.js
+++ js/principal.js
@@ -1,4 +1,21 @@
 var titulo = document.querySelector(".titulo");
-console.log(titulo.textContent);
-
 titulo.textContent = "Aparecida Nutricionista"
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+// console.log(paciente);
+// console.log(tdPeso);
+// console.log(peso);
+// console.log(altura);
+
+var tdImc = paciente.querySelector(".info-imc");
+
+var imc = peso / (altura * altura);
+
+tdImc.textContent = imc
