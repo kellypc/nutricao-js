78ec39cb3d16818d8770794a72c2ec8c45de7331
Kelly Cruz <kelly.cruz@rebase.com.br>
2021-04-26 17:41:19 -0300

    Aplicando o innerHTML e usando o forEach

--- js/form.js
+++ js/form.js
@@ -13,8 +13,7 @@ botaoAdicionar.addEventListener("click", function(event){
   var erros = validaPaciente(paciente)
 
   if(erros.length > 0){
-    var mensagemErro = document.querySelector("#mensagem-erro");
-    mensagemErro.textContent = erro;
+    exibeMensagensDeErro(erros);
     return;
   }
   //Adiciona paciente a tabela
@@ -23,6 +22,8 @@ botaoAdicionar.addEventListener("click", function(event){
   tabela.appendChild(pacienteTr);
 
   form.reset();
+  var mensagensErro = document.querySelector("#mensagens-erro");
+  mensagensErro.innerHTML = "";
 });
 // Estamos capturando o evento de click com a função anonima, no exemplo abaixo tem a funçao nomeada(desaclopada)
 //estamos escultando o evento de click no titulo através do addEventListener
@@ -32,7 +33,15 @@ botaoAdicionar.addEventListener("click", function(event){
 // function mostraMensagem(){
 //   console.log("Olá, eu fui clicado!");
 // }
-
+function exibeMensagensDeErro(erros){
+  var ul = document.querySelector("#mensagens-erro");
+  ul.innerHTML = "";
+  erros.forEach(function(erro){
+    var li = document.createElement("li");
+    li.textContent = erro;
+    ul.appendChild(li);
+  });
+}
 function obtemPacienteDoFormulario(form){
   //cria um objeto paciente com  as suas propriedades
   var paciente = {
@@ -71,6 +80,22 @@ function montaTd(dado,classe){
 function validaPaciente(paciente){
 
   var erros = [];
+
+  if (paciente.nome.length == 0) {
+    erros.push("O nome do paciente não pode ser em branco");
+  }
+
+  if (paciente.gordura.length == 0) {
+    erros.push("A gordura não pode ser em branco");
+  }
+
+  if (paciente.peso.length == 0) {
+    erros.push("O peso não pode ser em branco");
+  }
+
+  if (paciente.altura.length == 0) {
+    erros.push("A altura não pode ser em branco");
+  }
 
   if(!validaPeso(paciente.peso)){
     erros.push("Peso é inválido");
