dec721780a3131c8e869014caf6d5b83d1784ae8
Kelly Cruz <kelly.cruz@rebase.com.br>
2021-04-27 11:07:45 -0300

    Usando a funçao Event Bubbling para remover a partir do topo da tabela

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,7 +1,19 @@
 var pacientes = document.querySelectorAll(".paciente");
 
-pacientes.forEach(function(paciente){
-  paciente.addEventListener("dblclick", function(){ //doubleclick por isso o dblclick
-    this.remove();
+var tabela = document.querySelector("table");
+
+tabela.addEventListener("dblclick", function(event){
+  var alvoEvento = event.target; //tabela
+  var paiDoAlvo = alvoEvento.parentNode; //tr = paciente = remover
+  paiDoAlvo.remove();
   });
 });
+//Conseguimos colocar em uma só linha esse código:
+// event.target.parentNode.revome();
+//a funçao parentNote é chamada de event bubbling onde ao inves de fazermos como o código com o
+//forEach que está abaixo, nós escutamos o evento pelo pai(tabela) e removemos o filho(td)
+// pacientes.forEach(function(paciente){
+//   paciente.addEventListener("dblclick", function(){ //doubleclick por isso o dblclick
+//     this.remove();
+//   });
+// });
