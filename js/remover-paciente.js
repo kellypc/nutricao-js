1f98e4077065c5717dad1340e7a7501c40051b0c
Kelly Cruz <kelly.cruz@rebase.com.br>
2021-04-28 11:11:08 -0300

    Adiciona animação ao remover paciente com o setTimeout

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -3,14 +3,17 @@ var pacientes = document.querySelectorAll(".paciente");
 var tabela = document.querySelector("table");
 
 tabela.addEventListener("dblclick", function(event){
-  var alvoEvento = event.target; //tabela
-  var paiDoAlvo = alvoEvento.parentNode; //tr = paciente = remover
-  paiDoAlvo.remove();
-  });
+  event.target.parentNode.classList.add("fadeOut");
+
+  setTimeout(function(){
+    event.target.parentNode.remove();
+  }, 500);
 });
 //Conseguimos colocar em uma só linha esse código:
-// event.target.parentNode.revome();
-//a funçao parentNote é chamada de event bubbling onde ao inves de fazermos como o código com o
+    // var alvoEvento = event.target; //tabela
+    // var paiDoAlvo = alvoEvento.parentNode; //tr = paciente = remover
+    // paiDoAlvo.remove();
+//a funçao parentNote é chamada de Event bubbling onde ao inves de fazermos como o código com o
 //forEach que está abaixo, nós escutamos o evento pelo pai(tabela) e removemos o filho(td)
 // pacientes.forEach(function(paciente){
 //   paciente.addEventListener("dblclick", function(){ //doubleclick por isso o dblclick
